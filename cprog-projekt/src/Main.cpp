e0f0e9bdb520827c8aedf805b031a831572785e5
MathildaLindkvist <mathilda@qns.se>
2024-11-29 20:20:20 +0100

    Allt funkar men fastnar i spelet

--- cprog-projekt/src/Main.cpp
+++ cprog-projekt/src/Main.cpp
@@ -1,6 +1,15 @@
+#include "Session.h"
+#include "Sprite.h"
+#include "Component.h"
+//#include <SDL2/SDL.h>
 
-int main (){
+using namespace engine;
 
+int main (int argc, char** argv){
+
+   Sprite* testSprite = Sprite::getInstance(100, 100, 100, 100);
+   session.addComponent(testSprite);
+   session.run();
 
     return 0;
 }
