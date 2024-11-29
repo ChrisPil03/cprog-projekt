7216bf8f24e419181aeeb70d86962a7ef6a19ed5
MathildaLindkvist <mathilda@qns.se>
2024-11-29 20:37:26 +0100

    Can exit game

--- cprog-projekt/src/Session.cpp
+++ cprog-projekt/src/Session.cpp
@@ -23,6 +23,7 @@ void Session::run() {
         SDL_Event event;
         while (SDL_PollEvent(&event)){
             switch(event.type){
+                case SDL_QUIT: quit = true; break;
                 case SDL_KEYDOWN:
                     switch (event.key.keysym.sym){
                         case SDLK_RIGHT:
