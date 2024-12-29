a3f686dcd76c9bec7455e9c49bd17b85728e2b88
ChrisPil03 <chris@pilegard.se>
2024-12-29 17:51:38 +0100

    platforms

--- cprog-projekt/include/Game_Platform.h
+++ cprog-projekt/include/Game_Platform.h
@@ -10,17 +10,18 @@ namespace game
     class Platform : public engine::Sprite
     {
     public:
-        static Platform* getInstance(int x, int y, int w, int h, std::string imagePath, std::string tag);
+        static Platform* getInstance(int x, int y, int w, int h, std::string imagePath, std::string tag, bool horizontal, int colPos1, int colPos2);
         ~Platform();
         void update();
         const int getSpeed(){ return speed; }
 
     private:
-        Platform(int x, int y, int w, int h, std::string imagePath, std::string tag);
+        Platform(int x, int y, int w, int h, std::string imagePath, std::string tag, bool horizontal, int colPos1, int colPos2);
         engine::Collider2D* collider;
         engine::Collider2D* target1;
         engine::Collider2D* target2;
         int speed = 1;
+        bool horizontalMovement;
     };
 }
 
--- cprog-projekt/src/Game_Platform.cpp
+++ cprog-projekt/src/Game_Platform.cpp
@@ -2,20 +2,29 @@
 
 namespace game
 {
-    Platform* Platform::getInstance(int x, int y, int w, int h, std::string imagePath, std::string tag){
-        return new Platform(x,y,w,h,imagePath,tag);
+    Platform* Platform::getInstance(int x, int y, int w, int h, std::string imagePath, std::string tag, bool horizontal, int colPos1, int colPos2){
+        return new Platform(x,y,w,h,imagePath,tag,horizontal,colPos1,colPos2);
     }
 
-    Platform::Platform(int x, int y, int w, int h, std::string imagePath, std::string tag) : 
-        Sprite(x,y,w,h,imagePath), collider(engine::Collider2D::getInstance(x,y,w,h,tag)),
-        target1(engine::Collider2D::getInstance(x+60+w,y,1, 10, "Target")),
-        target2(engine::Collider2D::getInstance(x-60,y,1, 10, "Target"))
+    Platform::Platform(int x, int y, int w, int h, std::string imagePath, std::string tag, bool horizontal, int colPos1, int colPos2) : 
+        Sprite(x,y,w,h,imagePath), collider(engine::Collider2D::getInstance(x,y,w,h,tag)), horizontalMovement(horizontal)
     {
+        if (horizontal){
+            target1 = engine::Collider2D::getInstance(x-colPos1,y,1, 1, "Target");
+            target2 = engine::Collider2D::getInstance(x+w+colPos1,y,1, 1, "Target");
+        }else{
+            target1 = engine::Collider2D::getInstance(x,y-colPos1,1, 1, "Target");
+            target2 = engine::Collider2D::getInstance(x,y+h+colPos2,1, 1, "Target");
+        }
         collider->setParent(this);
     }
 
-    void Platform::update(){      
-        getRect()->x += speed;
+    void Platform::update(){  
+        if (horizontalMovement){
+            getRect()->x += speed;
+        }else{
+            getRect()->y += speed;
+        }    
 
         if(collider->hasCollided("Target")){
             speed *= -1;
